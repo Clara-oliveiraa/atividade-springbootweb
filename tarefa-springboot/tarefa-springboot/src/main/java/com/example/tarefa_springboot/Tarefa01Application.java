5e79e08fd6667a84c558a020afd2c02eadeb8611
Clara Oliveira <claraoliveira.8351@gmail.com>
2024-08-13 20:58:37 -0300

    SpringBoot
    Atividade de Márcio 01

---
+++ tarefa-springboot/tarefa-springboot/src/main/java/com/example/tarefa_springboot/Tarefa01Application.java
@@ -0,0 +1,13 @@
+package com.example.tarefa_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarefa01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarefa01Application.class, args);
+	}
+
+}
